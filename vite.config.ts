caf873daae46b79aa7412d956c7adc5bd8880322
Shashank Raju <shashankrajuprathikantam@gmail.com>
2025-05-29 00:06:45 +0530

    Added MUI with pigment CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { createTheme } from "@mui/material";
+import { pigment } from "@pigment-css/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+const pigmentConfig = {
+  theme: createTheme({
+    cssVariables: true,
+    colorSchemes: { light: true, dark: true },
+  }),
+  transformLibraries: ["@mui/material"],
+};
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), pigment(pigmentConfig)],
+  optimizeDeps: {
+    include: ["prop-types", "react-is", "hoist-non-react-statics"],
+  },
+});
